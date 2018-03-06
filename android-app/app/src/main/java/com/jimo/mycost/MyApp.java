978403af010c6708ab5c902b717ed32a0ad80519
jimolonely <wp147188@foxmail.com>
2018-03-06 14:58:57 +0800

    update version

--- android-app/app/src/main/java/com/jimo/mycost/MyApp.java
+++ android-app/app/src/main/java/com/jimo/mycost/MyApp.java
@@ -15,43 +15,44 @@ import java.io.File;
 
 public class MyApp extends Application {
 
+    static DbManager.DaoConfig daoConfig;
+    public static DbManager dbManager;
+
     @Override
     public void onCreate() {
         super.onCreate();
         x.Ext.init(this);
         x.Ext.setDebug(false); //输出debug日志，开启会影响性能
+        daoConfig = new DbManager.DaoConfig()
+                .setDbName("mycost.db")
+                // 不设置dbDir时, 默认存储在app的私有目录.
+//            .setDbDir(new File("/mnt/sdcard"))
+                .setDbVersion(2)
+                .setDbOpenListener(new DbManager.DbOpenListener() {
+                    @Override
+                    public void onDbOpened(DbManager db) {
+                        // 开启WAL, 对写入加速提升巨大
+                        db.getDatabase().enableWriteAheadLogging();
+                    }
+                })
+                .setDbUpgradeListener(new DbManager.DbUpgradeListener() {
+                    @Override
+                    public void onUpgrade(DbManager db, int oldVersion, int newVersion) {
+                        // TODO: ...
+                        // db.addColumn(...);
+                        // db.dropTable(...);
+                        // ...
+                        // or
+                        // db.dropDb();
+                    }
+                }).setTableCreateListener(new DbManager.TableCreateListener() {
+                    @Override
+                    public void onTableCreated(DbManager db, TableEntity<?> table) {
+                        Log.i("创建表", table.getName());
+                    }
+                });
+        dbManager = x.getDb(daoConfig);
     }
 
-    static DbManager.DaoConfig daoConfig = new DbManager.DaoConfig()
-            .setDbName("mycost.db")
-            // 不设置dbDir时, 默认存储在app的私有目录.
-            .setDbDir(new File("/mnt/sdcard"))
-            .setDbVersion(2)
-            .setDbOpenListener(new DbManager.DbOpenListener() {
-                @Override
-                public void onDbOpened(DbManager db) {
-                    // 开启WAL, 对写入加速提升巨大
-                    db.getDatabase().enableWriteAheadLogging();
-                }
-            })
-            .setDbUpgradeListener(new DbManager.DbUpgradeListener() {
-                @Override
-                public void onUpgrade(DbManager db, int oldVersion, int newVersion) {
-                    // TODO: ...
-                    // db.addColumn(...);
-                    // db.dropTable(...);
-                    // ...
-                    // or
-                    // db.dropDb();
-                }
-            }).setTableCreateListener(new DbManager.TableCreateListener() {
-                @Override
-                public void onTableCreated(DbManager db, TableEntity<?> table) {
-                    Log.i("创建表", table.getName());
-                }
-            });
-
-
-    public static DbManager dbManager = x.getDb(daoConfig);
 
 }
