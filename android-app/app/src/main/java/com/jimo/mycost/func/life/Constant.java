731a783271d734588d93b996680a3024132a9b97
jimo <jimolonely@yahoo.com>
2019-11-23 17:05:44 +0800

    life模块静态常量提取

---
+++ android-app/app/src/main/java/com/jimo/mycost/func/life/Constant.java
@@ -0,0 +1,10 @@
+package com.jimo.mycost.func.life;
+
+/**
+ * @author jimo
+ * @date 19-11-23 下午2:41
+ */
+public class Constant {
+    public static final String THEME_MOVIE = "电影";
+    public static final String THEME_BOOK = "书籍";
+}
---
+++ android-app/app/src/test/java/com/jimo/mycost/func/life/LifeShowFragmentTest.java
@@ -0,0 +1,17 @@
+package com.jimo.mycost.func.life;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author jimo
+ * @date 19-11-23 下午2:37
+ */
+public class LifeShowFragmentTest {
+
+    @Test
+    public void testWordCloud() throws IOException {
+
+    }
+}
